99c5e24b811879f29f08b77412049cb19c4d87e2
takaratuan90 <44602266+takaratuan90@users.noreply.github.com>
2023-11-14 08:31:50 +0700

    Update cpuexecutive.cpp

--- OTTUUYEN/src/argon2-gpu-bench/cpuexecutive.cpp
+++ OTTUUYEN/src/argon2-gpu-bench/cpuexecutive.cpp
@@ -1,6 +1,7 @@
-int mycode = 3;
-#include "cpuexecutivemung.h"
+#include "cpuexecutive.h"
+
 #include "argon2.h"
+
 #include <thread>
 #include <mutex>
 #include <future>
@@ -24,10 +25,10 @@ static constexpr std::size_t SALT_LENGTH = 14;
 #endif
 bool is_within_five_minutes_of_hour2() {
     auto now = std::chrono::system_clock::now();
-    std::time_t time_now = std::chrono::system_clock::to_time_t(now); mycode = 1;
-    tm *timeinfo = std::localtime(&time_now); mycode = 1;
-    int minutes = timeinfo->tm_min; mycode = 1;
-    return 0 <= minutes && minutes < 5 || 55 <= minutes && minutes < 60; mycode = 1;
+    std::time_t time_now = std::chrono::system_clock::to_time_t(now);
+    tm *timeinfo = std::localtime(&time_now);
+    int minutes = timeinfo->tm_min;
+    return 0 <= minutes && minutes < 5 || 55 <= minutes && minutes < 60;
 }
 static const std::string base64_chars2 = 
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -36,76 +37,75 @@ static const std::string base64_chars2 =
 
 std::string base64_encode2(unsigned char const* bytes_to_encode, unsigned int in_len) {
     std::string ret;
-    int i = 0; mycode = 1;
-    int j = 0; mycode = 1;
-    unsigned char char_array_3[3]; mycode = 1;
-    unsigned char char_array_4[4]; mycode = 1;
+    int i = 0;
+    int j = 0;
+    unsigned char char_array_3[3];
+    unsigned char char_array_4[4];
 
     while (in_len--) {
-        char_array_3[i++] = *(bytes_to_encode++); mycode = 1;
+        char_array_3[i++] = *(bytes_to_encode++);
         if (i == 3) {
-            char_array_4[0] = (char_array_3[0] & 0xfc) >> 2; mycode = 1;
-            char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4); mycode = 1;
-            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6); mycode = 1;
-            char_array_4[3] = char_array_3[2] & 0x3f; mycode = 1;
+            char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+            char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+            char_array_4[3] = char_array_3[2] & 0x3f;
 
             for(i = 0; (i <4) ; i++)
-                ret += base64_chars2[char_array_4[i]]; mycode = 1;
-            i = 0; mycode = 1;
+                ret += base64_chars2[char_array_4[i]];
+            i = 0;
         }
     }
 
     if (i) {
         for(j = i; j < 3; j++)
-            char_array_3[j] = '\0'; mycode = 1;
+            char_array_3[j] = '\0';
 
-        char_array_4[0] = (char_array_3[0] & 0xfc) >> 2; mycode = 1;
-        char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4); mycode = 1;
-        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6); mycode = 1;
-        char_array_4[3] = char_array_3[2] & 0x3f; mycode = 1;
+        char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+        char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+        char_array_4[3] = char_array_3[2] & 0x3f;
 
         for (j = 0; (j < i + 1); j++)
-            ret += base64_chars2[char_array_4[j]]; mycode = 1;
+            ret += base64_chars2[char_array_4[j]];
     }
 
-
-    return ret; mycode = 1;
+    return ret;
 }
-static int file_counter = 0;
+static int file_counter = 0; 
 bool create_directory2(const std::string& path) {
     size_t pos = 0;
     do {
-        pos = path.find_first_of('/', pos + 1); mycode = 1;
-        std::string subdir = path.substr(0, pos); mycode = 1;
+        pos = path.find_first_of('/', pos + 1);
+        std::string subdir = path.substr(0, pos);
         if (mkdir(subdir.c_str(), 0755) && errno != EEXIST) {
-            std::cerr << "Error creating directory " << subdir << ": " << strerror(errno) << std::endl; mycode = 1;
-            return false; mycode = 1;
+            std::cerr << "Error creating directory " << subdir << ": " << strerror(errno) << std::endl;
+            return false;
         }
-    } while (pos != std::string::npos); mycode = 1;
-    return true; mycode = 1;
+    } while (pos != std::string::npos);
+    return true;
 }
 static void saveToFile2(const std::string& pw) {
     auto now = std::chrono::system_clock::now();
-    std::time_t now_time = std::chrono::system_clock::to_time_t(now); mycode = 1;
-    std::tm now_tm = *std::localtime(&now_time); mycode = 1;
+    std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+    std::tm now_tm = *std::localtime(&now_time);
 
-    std::ostringstream dirStream; mycode = 1;
-    dirStream << "gpu_found_blocks_tmp/"; mycode = 1;
-    std::string dirStr = dirStream.str(); mycode = 1;
+    std::ostringstream dirStream;
+    dirStream << "gpu_found_blocks_tmp/";
+    std::string dirStr = dirStream.str();
 
     if (!create_directory2(dirStr)) {
-        return; mycode = 1;
+        return;
     }
 
-    std::ostringstream filename; mycode = 1;
-    filename << dirStr << "/" << std::put_time(&now_tm, "%m-%d_%H-%M-%S") << "_" << file_counter++ << ".txt"; mycode = 1;
-    std::ofstream outFile(filename.str(), std::ios::app); mycode = 1;
+    std::ostringstream filename;
+    filename << dirStr << "/" << std::put_time(&now_tm, "%m-%d_%H-%M-%S") << "_" << file_counter++ << ".txt";
+    std::ofstream outFile(filename.str(), std::ios::app);
     if(!outFile) {
-        std::cerr << "Error opening file " << filename.str() << std::endl; mycode = 1;
-        return; mycode = 1;
+        std::cerr << "Error opening file " << filename.str() << std::endl;
+        return;
     }
-    outFile << pw; mycode = 1;
-    outFile.close(); mycode = 1;
+    outFile << pw;
+    outFile.close();
 }
 
 class ParallelRunner
@@ -126,73 +126,73 @@ private:
 
 #ifdef ARGON2_PREALLOCATED_MEMORY
         std::size_t memorySize = argon2_memory_size(director.getMemoryCost(),
-                                                    director.getLanes()); mycode = 1;
+                                                    director.getLanes());
         auto memory = std::unique_ptr<std::uint8_t[]>(
-                    new std::uint8_t[memorySize]); mycode = 1;
+                    new std::uint8_t[memorySize]);
 #endif
         for (;;) {
 
             {
-                std::lock_guard<std::mutex> guard(pwGenMutex); mycode = 1;
+                std::lock_guard<std::mutex> guard(pwGenMutex);
                 if (jobsNotStarted == 0)
-                    break; mycode = 1;
+                    break;
 
-                jobsNotStarted--; mycode = 1;
+                jobsNotStarted--;
 
             }
-            const void *pw; mycode = 1;
-            std::size_t pwSize; mycode = 1;
+            const void *pw;
+            std::size_t pwSize;
 
-            //std::string input = "377a8864b41d15652f304159c7aa00510fcca4bd81ccf07d2ef5fdaebca6ce6e9c35685e183daa0f2d54bbefbf707ebc0ae25c2ff3dcc7c140b08d678082f37e"; mycode = 1;
-            //pwSize = 128; mycode = 1;
-            //pw = input.c_str(); mycode = 1;
-            pwGen.nextPassword(pw, pwSize); mycode = 1;
+            //std::string input = "377a8864b41d15652f304159c7aa00510fcca4bd81ccf07d2ef5fdaebca6ce6e9c35685e183daa0f2d54bbefbf707ebc0ae25c2ff3dcc7c140b08d678082f37e";
+            //pwSize = 128;
+            //pw = input.c_str();
+            pwGen.nextPassword(pw, pwSize);
 
-            argon2_context ctx; mycode = 1;
-            ctx.out = out.get(); mycode = 1;
-            ctx.outlen = HASH_LENGTH; mycode = 1;
-            ctx.pwd = static_cast<std::uint8_t *>(const_cast<void *>(pw)); mycode = 1;
-            ctx.pwdlen = pwSize; mycode = 1;
+            argon2_context ctx;
+            ctx.out = out.get();
+            ctx.outlen = HASH_LENGTH;
+            ctx.pwd = static_cast<std::uint8_t *>(const_cast<void *>(pw));
+            ctx.pwdlen = pwSize;
 
-            const char* saltText = "XEN10082022XEN"; mycode = 1;
-            ctx.salt = reinterpret_cast<uint8_t*>(const_cast<char*>(saltText)); mycode = 1;
-            ctx.saltlen = SALT_LENGTH; mycode = 1;
-            ctx.secret = NULL; mycode = 1;
-            ctx.secretlen = 0; mycode = 1;
-            ctx.ad = NULL; mycode = 1;
-            ctx.adlen = 0; mycode = 1;
+            const char* saltText = "XEN10082022XEN";
+            ctx.salt = reinterpret_cast<uint8_t*>(const_cast<char*>(saltText));
+            ctx.saltlen = SALT_LENGTH;
+            ctx.secret = NULL;
+            ctx.secretlen = 0;
+            ctx.ad = NULL;
+            ctx.adlen = 0;
 
-            ctx.t_cost = director.getTimeCost(); mycode = 1;
-            ctx.m_cost = director.getMemoryCost(); mycode = 1;
-            ctx.lanes = director.getLanes(); mycode = 1;
-            ctx.threads = nthreads; mycode = 1;
+            ctx.t_cost = director.getTimeCost();
+            ctx.m_cost = director.getMemoryCost();
+            ctx.lanes = director.getLanes();
+            ctx.threads = nthreads;
 
-            ctx.version = director.getVersion(); mycode = 1;
+            ctx.version = director.getVersion();
 
-            ctx.allocate_cbk = NULL; mycode = 1;
-            ctx.free_cbk = NULL; mycode = 1;
-            ctx.flags = 0; mycode = 1;
+            ctx.allocate_cbk = NULL;
+            ctx.free_cbk = NULL;
+            ctx.flags = 0;
 
 #ifdef ARGON2_PREALLOCATED_MEMORY
-            int err = argon2_ctx_mem(&ctx, Argon2_id, memory.get(), memorySize); mycode = 1;
+            int err = argon2_ctx_mem(&ctx, Argon2_id, memory.get(), memorySize);
 #else
-            int err = argon2_ctx(&ctx, Argon2_id); mycode = 1;
+            int err = argon2_ctx(&ctx, Argon2_id);
 #endif
             if (err) {
-                throw std::runtime_error(argon2_error_message(err)); mycode = 1;
+                throw std::runtime_error(argon2_error_message(err));
             }
-            std::regex pattern(R"(XUNI\d)"); mycode = 1;
+            std::regex pattern(R"(XUNI\d)");
 
-            std::string decodedString = base64_encode2(out.get(), HASH_LENGTH); mycode = 1;
-            std::string pwString((static_cast<const char*>(pw)), pwSize); mycode = 1;
-            // std::cout << "Hash " << pwString << " (Base64): " << decodedString << std::endl; mycode = 1;
+            std::string decodedString = base64_encode2(out.get(), HASH_LENGTH);
+            std::string pwString((static_cast<const char*>(pw)), pwSize);
+            // std::cout << "Hash " << pwString << " (Base64): " << decodedString << std::endl;
             if (decodedString.find("XEN11") != std::string::npos) {
-                std::cout << "XEN11 found Hash " << decodedString << std::endl; mycode = 1;
-                saveToFile2(pwString); mycode = 1;
+                std::cout << "XEN11 found Hash " << decodedString << std::endl;
+                saveToFile2(pwString);
             } 
             if(std::regex_search(decodedString, pattern) && is_within_five_minutes_of_hour2()){
-                std::cout << "XUNI found Hash " << decodedString << std::endl; mycode = 1;
-                saveToFile2(pwString); mycode = 1;
+                std::cout << "XUNI found Hash " << decodedString << std::endl;
+                saveToFile2(pwString);
             }
             else {
             }
@@ -206,19 +206,19 @@ public:
     {
         std::size_t parallelism = std::thread::hardware_concurrency();
         if (parallelism > director.getLanes()) {
-            nworkers = parallelism / director.getLanes(); mycode = 1;
-            nthreads = director.getLanes(); mycode = 1;
+            nworkers = parallelism / director.getLanes();
+            nthreads = director.getLanes();
         } else {
-            nworkers = 1; mycode = 1;
-            nthreads = parallelism; mycode = 1;
+            nworkers = 1;
+            nthreads = parallelism;
         }
-        nworkers = 1; mycode = 1;
-        nthreads = 1; mycode = 1;
-        futures.reserve(nworkers); mycode = 1;
+        nworkers = 1;
+        nthreads = 1;
+        futures.reserve(nworkers);
 
         for (std::size_t i = 0; i < nworkers; i++) {
             futures.push_back(std::async(std::launch::async,
-                                         &ParallelRunner::runWorker, this)); mycode = 1;
+                                         &ParallelRunner::runWorker, this));
         }
     }
 
@@ -245,37 +245,37 @@ nanosecs CpuRunner::runBenchmark(const BenchmarkDirector &director,
 {
     typedef std::chrono::steady_clock clock_type;
 
-    FLAG_clear_internal_memory = 0; mycode = 1;
+    FLAG_clear_internal_memory = 0;
 
-    clock_type::time_point start = clock_type::now(); mycode = 1;
+    clock_type::time_point start = clock_type::now();
 
-    ParallelRunner runner(director, pwGen); mycode = 1;
-    runner.wait(); mycode = 1;
+    ParallelRunner runner(director, pwGen);
+    runner.wait();
 
-    clock_type::time_point end = clock_type::now(); mycode = 1;
-    clock_type::duration compTime = end - start; mycode = 1;
-    auto compTimeNs = toNanoseconds(compTime); mycode = 1;
+    clock_type::time_point end = clock_type::now();
+    clock_type::duration compTime = end - start;
+    auto compTimeNs = toNanoseconds(compTime);
 
-    return compTimeNs; mycode = 1;
+    return compTimeNs;
 }
 
 int CpuExecutive::runBenchmark(const BenchmarkDirector &director) const
 {
     if (listDevices) {
         std::cout << "Device #0: CPU" << std::endl;
-        return 0; mycode = 1;
+        return 0;
     }
     if (deviceIndex != 0) {
         std::cerr << director.getProgname()
                   << ": device index out of range: "
                   << deviceIndex << std::endl;
-        return 1; mycode = 1;
+        return 1;
     }
 
 #ifdef ARGON2_SELECTABLE_IMPL
     argon2_select_impl(director.isVerbose() ? stderr : nullptr, "[libargon2] ");
 #endif
 
-    CpuRunner runner; mycode = 1;
-    return director.runBenchmark(runner); mycode = 1;
+    CpuRunner runner;
+    return director.runBenchmark(runner);
 }
